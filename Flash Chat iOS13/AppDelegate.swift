af1b6e8321d1097273f6208697cf787f139b934d
khuluz <lindokuhle.zungu@outlook.com>
2023-02-01 18:47:19 +0200

    Flash-Chat

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import IQKeyboardManager
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+
+        Firestore.firestore()
+        IQKeyboardManager.shared().shouldResignOnTouchOutside = true
+        IQKeyboardManager.shared().isEnableAutoToolbar = false
+                
         return true
     }
 
--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,15 +7,28 @@
 //
 
 import UIKit
+import CLTypingLabel
+
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        titleLabel.text = "⚡️FlashChat"
+        
+//        var indexChar = 0.0
+//        titleLabel.text = ""
+//        let titleText = "⚡️FlashChat"
+//        for letter in titleText{
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * indexChar, repeats: false) { (timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            indexChar += 1
+//        }
+        
     }
     
 
